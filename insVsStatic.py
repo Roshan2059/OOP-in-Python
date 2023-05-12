5be79d64f540e7f2704113b4861bb37643e1597d
Roshan Panta <roshanpanta2059@gmail.com>
2023-05-12 14:48:36 +0545

    added instance vs static variables

---
+++ insVsStatic.py
@@ -0,0 +1,21 @@
+class Student():
+    faculty = "BCA"
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    def details(self):
+        print("name", self.name)
+        print("faculty", self.faculty)
+        print("age", self.age)
+        print("gender", self.gender)
+
+stu1 = Student("Roshan", 20, "male")
+stu1.details()
+stu2 = Student("Asmita", 21, "female")
+stu2.details()
+stu1.name = "sudip"
+Student.faculty = "BIM"
+stu1.details()
+stu2.details()
+
